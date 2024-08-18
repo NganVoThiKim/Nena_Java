f995d6d1148ccecd98367f7e3b522ee2621f2d9d
NganVo <nganvothikim270496@gmail.com>
2024-08-18 11:59:21 +0700

    The first commit

---
+++ src/VariablePractice.java
@@ -0,0 +1,10 @@
+public class VariablePractice {
+    public static void main(String[] args){
+        final int var = 7;
+        System.out.println("Constant variable: " + var);
+        // Constant is concept, final is keyword
+
+        String name = "John";
+        System.out.println("Hello " + name);
+    }
+}
